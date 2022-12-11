335a34b2b11de855d13a81c33cb65fb40c0e6499
Orimoloyetolulope <tohluh122@gmail.com>
2022-12-11 14:06:04 -0800

    My first C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - printing my first C program using puts function
+ *
+ * return: 0
+ *
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
