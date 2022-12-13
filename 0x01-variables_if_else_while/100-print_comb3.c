f56072c099e1f0f4d10aa1c0145dcac617e54918
Orimoloyetolulope <tohluh122@gmail.com>
2022-12-12 16:34:32 -0800

    C program is stressful

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,13 +6,12 @@
  * code by Tohluh122
  * Return: 0
  */
-int main (void)
+int main(void)
 {
 	int digit1, digit2;
 
 	for (digit1 = 0; digit1 < 9; digit1++)
-	{
-		
+	{		
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
 			putchar((digit1 % 10) + '0');
