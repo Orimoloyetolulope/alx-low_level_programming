c8ebcd38d855ed300d6a43d1a4251601e5f4ede9
Orimoloyetolulope <tohluh122@gmail.com>
2022-12-14 15:53:46 -0800

    Prints every minute of the day of Jack Bauer.

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,12 +5,11 @@
  * 00:00 to 23:59.
  *
  */
-
 void jack_bauer(void)
 {
 	int hour, minute;
 
-	for (hour = 0; hour <= 23; minute++)
+	for (hour = 0; hour <= 23; hour++)
 	{
 		for (minute = 0; minute <= 59; minute++)
 		{
