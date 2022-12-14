16f1b4a0dc7691acad0d08c739392df212a21d5a
Orimoloyetolulope <tohluh122@gmail.com>
2022-12-14 13:21:29 -0800

    Prints the alphabet in lowercase folled by a new line.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - point all alphabet in lowercase
+ * code by Tohluh122
+ * Return: 0
+ *
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+
+}
