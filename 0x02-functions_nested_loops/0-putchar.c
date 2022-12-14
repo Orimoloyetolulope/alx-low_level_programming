8bf3ccf6438b4f68ca8513519d9799eef3719266
Orimoloyetolulope <tohluh122@gmail.com>
2022-12-14 12:40:08 -0800

    Prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Print putchar
+ * code by Tohluh122
+ * Return: 0
+ */
+
+int main(void)
+{
+	-putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
