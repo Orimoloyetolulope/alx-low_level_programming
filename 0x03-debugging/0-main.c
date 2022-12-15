077fc825573a77f9b9148912ff0784888aba3b3e
Orimoloyetolulope <tohluh122@gmail.com>
2022-12-15 15:18:49 -0800

    My tasks under debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Tests the function that prints if int is positive or negative.
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	int 0;
+	positive_or_negative(i);
+
+	return (0);
+
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+This is my task on debugging and i must confess alx is not a beans at all.
+Thank God anyway.
