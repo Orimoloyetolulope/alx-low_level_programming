5cb777fb1c566b53236390aec108d8eceb130423
Orimoloyetolulope <tohluh122@gmail.com>
2022-12-15 15:40:34 -0800

    My debugging task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Tests the function that prints if int is positive or negative
+ * main - Tests the function that prints if integer is positive or negative
  * Return: 0
  */
 
