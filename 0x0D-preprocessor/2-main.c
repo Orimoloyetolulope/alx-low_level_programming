5f295771b19cf6d20acdbb3167c9dace702069ff
Orimoloyetolulope <tohluh122@gmail.com>
2023-01-17 12:24:35 -0800

    file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,14 +2,13 @@
 
 /**
  * main - Prints the name of the file of the program
- *	was compiled from, followed by a new line.
+ *        was compiled from, followed by a new line.
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-	printf("%s\n", _FILE_);
+	printf("%s\n", __FILE__);
 
 	return (0);
 }
